3ec2b2a4b9498090a598787d6ce0f9e7676dc9a8
knepharyus1 <red_disk_icon@yahoo.com>
2015-10-06 16:11:32 -0400

    syntax corrections

--- cmd-opts.sh
+++ cmd-opts.sh
@@ -4,15 +4,26 @@ FILE=""
 PASSWD=""
 OUTPUTDIR=""
 
-while getopts :hf:p:d: opt; do
+usage() {
+cat <<EOF
+  ERROR: $1
+
+  Usage: cmd-opts <[opts]>
+  Options:
+    -d  output directory
+    -f  input file
+    -p  password
+EOF
+}
+
+while getopts :hd:f:p: opt; do
   case $opt in
     h)
       usage
       ;;
     f)
       FILE=$OPTARG
-      if [ ! -f $FILE ]
-      then
+      if [[ ! -f $FILE ]]; then
         echo "file $FILE does not exist"
         exit 1
       fi
@@ -22,8 +33,7 @@ while getopts :hf:p:d: opt; do
       ;;
     d)
       OUTPUTDIR=$OPTARG
-      if [ ! -d $OUTPUTDIR ]
-      then
+      if [[ ! -d $OUTPUTDIR ]]; then
         echo "director $OUTPUTDIR does not exist"
         exit 1
       fi
@@ -35,14 +45,16 @@ while getopts :hf:p:d: opt; do
   esac
 done
 
-usage() {
-  cat <<EOF
-    cmd-opts $opts
-    $*
-        Usage: cmd-opts <[opts]>
-        Options:
-                -d  output directory
-                -f  input file
-                -p  password
-  EOF
-}
+if [[ $FILE == "" ]] || [[ ! -f $FILE ]]; then
+  usage "no valid file provided"
+  exit 1
+fi
+
+if [[ $OUTPUTDIR == "" ]] || [[ ! -d $OUTPUTDIR ]]; then
+  usage "no valid output directory provided"
+  exit 1
+fi
+
+while read line; do
+  echo "the line is $line..."
+done < $FILE

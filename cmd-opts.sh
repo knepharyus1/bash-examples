a8d16d6c040cc7dc5fddda33c998f9418d952124
knepharyus1 <red_disk_icon@yahoo.com>
2015-10-07 16:56:41 -0400

    added loop examples

--- cmd-opts.sh
+++ cmd-opts.sh
@@ -2,18 +2,25 @@
 
 FILE=""
 PASSWD=""
-OUTPUTDIR=""
+DIR=""
 
 usage() {
-cat <<EOF
-  ERROR: $1
 
-  Usage: cmd-opts <[opts]>
+  if [[ $1 != "" ]]; then
+    echo "  ERROR: $1"
+  fi
+
+cat <<EOF
+
+  Usage: $0 <[opts]>
   Options:
     -d  output directory
     -f  input file
+    -h  display this help dialog
     -p  password
 EOF
+
+  exit 0
 }
 
 while getopts :hd:f:p: opt; do
@@ -23,20 +30,12 @@ while getopts :hd:f:p: opt; do
       ;;
     f)
       FILE=$OPTARG
-      if [[ ! -f $FILE ]]; then
-        echo "file $FILE does not exist"
-        exit 1
-      fi
       ;;
     p)
       PASSWD=$OPTARG
       ;;
     d)
-      OUTPUTDIR=$OPTARG
-      if [[ ! -d $OUTPUTDIR ]]; then
-        echo "director $OUTPUTDIR does not exist"
-        exit 1
-      fi
+      DIR=$OPTARG
       ;;
     \?)
       echo "invalid option: -$OPTARG"
@@ -45,16 +44,32 @@ while getopts :hd:f:p: opt; do
   esac
 done
 
+# Check for the existence of a file
 if [[ $FILE == "" ]] || [[ ! -f $FILE ]]; then
   usage "no valid file provided"
   exit 1
 fi
 
-if [[ $OUTPUTDIR == "" ]] || [[ ! -d $OUTPUTDIR ]]; then
+# Check for the existence of a directory
+if [[ $DIR == "" ]] || [[ ! -d $DIR ]]; then
   usage "no valid output directory provided"
   exit 1
 fi
 
+# Loop over lines in a file
 while read line; do
   echo "the line is $line..."
+
+  # Loop over words in a line
+  # Same as looping through array items
+  for word in $line; do
+    echo "the word is $word..."
+  done
+
 done < $FILE
+
+# Loop through contents of a directory
+for item in $(ls $DIR)
+do
+  echo "I found $item..."
+done

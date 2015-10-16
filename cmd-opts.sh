fbf33357b1f7ed013bdf948294a0c7d83d76d7e4
knepharyus1 <red_disk_icon@yahoo.com>
2015-10-16 10:51:58 -0400

    added fun with arrays, loops, and sed

--- cmd-opts.sh
+++ cmd-opts.sh
@@ -68,8 +68,59 @@ while read line; do
 
 done < $FILE
 
-# Loop through contents of a directory
+## Loop through contents of a directory
 for item in $(ls $DIR)
 do
   echo "I found $item..."
 done
+
+## Loop over a comma-separated list
+$list="foo,bar,baz"
+for item in $(echo $list | sed "s/,/ /g")
+do
+  echo $item
+done
+
+## Create an array. Also, increment a number variable
+count=0
+declare -A arr
+
+for item in $(echo $list | sed "s/,/ /g")
+do
+  arr+=( [$item]="entry number $count" )
+  ((count++))
+done
+
+for key in ${!arr[@]}
+do
+  echo "${key} is ${arr[${key}]}"
+done
+
+## Create a file with variable replacement enabled
+cat > /path/to/file.txt <<EOF
+line 1:
+line 2: the variable $DIR will be replaced with its assigned value
+EOF
+
+## Append to a file with variable replacement disabled
+cat >> /path/to/file.txt <<'EOF'
+line 3:
+line 4: the literal string $DIR will be written to the file, not the assigned value
+EOF
+
+## Append to a file with variable replacement enabled,
+## but also allowing inclusion of a variable name.
+## This shows that variable replacement is single-level
+TEMP='$varname'
+cat >> /path/to/file.txt <<EOF
+line 5:
+line 6: $DIR will be replaced with its assigned value
+line 7: $TEMP will as well, by the literal string dollarsignvarname
+EOF
+
+## In-place string replacement in a file
+sed -i 's/change me/i got changed/' /path/to/file.txt
+
+## In-place string replacement in a file, allowing variable
+## replacement, and using alternate separator
+sed -i -e "s@path=\"/a/file/path\"@path=\"/a/new/${DIR}\"@" /path/to/file.txt
